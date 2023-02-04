8a933b3f831e076d9b76aad4cd67db764f2cb77a
foramirez50 <98720686+foramirez50@users.noreply.github.com>
2023-02-03 21:33:21 -0500

    Update __init__.py

--- entrega0final/website/__init__.py
+++ entrega0final/website/__init__.py
@@ -42,5 +42,5 @@ def create_database(app):
         print('Created Database!')        
 
 
-#    if __name__ == "__main__":
-#    	app.run(host='0.0.0.0')
+    if __name__ == "__main__":
+    	app.run(host='0.0.0.0')
